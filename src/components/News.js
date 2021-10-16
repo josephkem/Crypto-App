14aa32f76839e2aa1242979f97bbd8ae6e0e2ae4
josephkem <josephkemoy@gmail.com>
2021-10-15 20:45:57 -0400

    Added jsx to render data from the news api

--- src/components/News.js
+++ src/components/News.js
@@ -9,13 +9,25 @@ const { Option } = Select;
 const News = ({ simplified }) => {
   const { data: cryptoNews } = useGetCryptoNewsQuery({
     newsCategory: "Cryptocurrency",
-    count: simplified ? 10 : 50,
+    count: simplified ? 6 : 12,
   });
-  console.log(cryptoNews);
+  if (!cryptoNews?.value) return "Loading . . .";
   return (
-    <div>
-      <h1>News</h1>
-    </div>
+    <Row gutter={[24, 24]}>
+      {cryptoNews.value.map((news, i) => (
+        <Col xs={24} sm={12} lg={8} key={i}>
+          <Card hoverable className="news-card">
+            <a href={news.url} target="_blank" rel="noreferrer">
+              <div className="news-image-container">
+                <Title className="news-title" level={4}>
+                  {news.name}
+                </Title>
+              </div>
+            </a>
+          </Card>
+        </Col>
+      ))}
+    </Row>
   );
 };
 
