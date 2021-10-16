fbf2d822e2f2373a2cfd5085c8ee2ab986094210
josephkem <josephkemoy@gmail.com>
2021-10-16 12:16:24 -0400

    Fixed an error that caused the project to break during refresh

--- src/components/CryptoDetails.js
+++ src/components/CryptoDetails.js
@@ -22,8 +22,9 @@ const { Option } = Select;
 const CryptoDetails = () => {
   const { coinId } = useParams();
   const [timePeriod, setTimePeriod] = useState("7d");
-  const { data, isFething } = useGetCryptoDetailsQuery(coinId);
+  const { data, isFetching } = useGetCryptoDetailsQuery(coinId);
   const cryptoDetails = data?.data?.coin;
+  if (isFetching) return "Loading . . .";
   console.log(data);
 
   const time = ["3h", "24h", "7d", "30d", "1y", "3m", "3y", "5y"];
@@ -104,6 +105,68 @@ const CryptoDetails = () => {
           <Option key={date}>{date}</Option>
         ))}
       </Select>
+      <Col className="stats-container">
+        <Col className="coin-value-statistics">
+          <Col className="coin-value-statistics-heading">
+            <Title level={3} className="coin-details-heading">
+              {cryptoDetails.name} Value Statistics
+            </Title>
+            <p>
+              An overview showing the statistics of {cryptoDetails.name}, such
+              as the base and quote currency, the rank, and trading volume.
+            </p>
+          </Col>
+          {stats.map(({ icon, title, value }) => (
+            <Col className="coin-stats">
+              <Col className="coin-stats-name">
+                <Text>{icon}</Text>
+                <Text>{title}</Text>
+              </Col>
+              <Text className="stats">{value}</Text>
+            </Col>
+          ))}
+        </Col>
+      </Col>
+      <Col className="other-stats-info">
+        <Col className="coin-value-statistics-heading">
+          <Title level={3} className="coin-details-heading">
+            Other Statistics
+          </Title>
+          <p>An overview showing the statistics of all cryptocurrencies.</p>
+        </Col>
+        {genericStats.map(({ icon, title, value }) => (
+          <Col className="coin-stats">
+            <Col className="coin-stats-name">
+              <Text>{icon}</Text>
+              <Text>{title}</Text>
+            </Col>
+            <Text className="stats">{value}</Text>
+          </Col>
+        ))}
+      </Col>
+      <Col className="coin-desc-link">
+        <Row className="coin-desc">
+          <Title level={3} className="coin-details-heading">
+            What is {cryptoDetails.name}?
+          </Title>
+          {HTMLReactParser(cryptoDetails.description)}
+        </Row>
+        <Col className="coin-links">
+          <Title level={3} className="coin-details-heading">
+            {cryptoDetails.name} Links
+          </Title>
+          {cryptoDetails.links?.map((link) => (
+            <Row className="coin-link" key={link.name}>
+              <Title level={5} className="link-name">
+                {link.type}
+              </Title>
+              <a href={link.url} target="_blank" rel="noreferrer">
+                {link.name}
+              </a>
+            </Row>
+          ))}
+        </Col>
+      </Col>
     </Col>
   );
 };
